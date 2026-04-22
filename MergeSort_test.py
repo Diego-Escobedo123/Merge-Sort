82cd242532fc4a286ba1cb088bef63d86b55d1fc
Cantthinkofabetterusername <pp3drolinar3s@gmail.com>
2026-04-22 11:14:47 -0600

    commit_test

---
+++ MergeSort_test.py
@@ -0,0 +1,9 @@
+unsorted_list = [8, 4, 1, 3, 7, 2, 5, 6]
+
+sorted_list = [1, 2, 3, 4, 5, 6, 7, 8]
+
+if unsorted_list == sorted_list:
+    print("La lista está ordenada")
+else:
+    print("La lista no está ordenada")
+
